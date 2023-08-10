6f1e6c3a3f325d2f94f0eef742143ab2673a6b32
unknown <bauerbrett1@gmail.com>
2023-08-10 16:21:48 -0400

    terraform test27

--- main.tf
+++ main.tf
@@ -11,11 +11,26 @@ terraform {
     
   }
 }
+variable "ARM_TENANT_ID" {
+  description = "Tenant ID"
+  type = string
+}
+variable "ARM_CLIENT_ID" {
+  description = "CLIENT ID"
+  type = string
+}
+variable "ARM_CLIENT_SECRET" {
+  description = "CLIENT SECRET"
+  type = string
+}
 # Resource Manager Default Sub
 provider "azurerm" {
   version = ">=2.0.0"
   features {}
   tenant_id       = "134888ca-e6f2-4fb6-9b49-3042d590ea87"
+  subsubscription_id = "3b8667c6-8f75-42ea-b301-bf27c9db8674"
+  client_id = var.ARM_CLIENT_ID
+  client_secret = var.ARM_CLIENT_SECRET
 }
 # Azure Resource Manager Timberwolve Prod
 provider "azurerm" {
@@ -26,8 +41,10 @@ provider "azurerm" {
       }
   alias = "timbdev"
   version = ">=2.0.0"
+  client_id = var.ARM_CLIENT_ID
+  client_secret = var.ARM_CLIENT_SECRET
   subscription_id = "c46a6ca5-2236-4b9d-aff2-e465190d0b3b"
-  tenant_id       = "134888ca-e6f2-4fb6-9b49-3042d590ea87"
+  tenant_id       = var.ARM_TENANT_ID
   skip_provider_registration = "true"
 }
 # Azure Resource Manager Warriors Prod
@@ -35,13 +52,17 @@ provider "azurerm" {
   alias = "wardev"
   version = ">=2.0.0"
   features {}
+  client_id = var.ARM_CLIENT_ID
+  client_secret = var.ARM_CLIENT_SECRET
   subscription_id = "50f1a1d8-806c-46ab-82f4-41fb54e730d8"
-  tenant_id       = "134888ca-e6f2-4fb6-9b49-3042d590ea87"
+  tenant_id       = var.ARM_TENANT_ID
   skip_provider_registration = "true"
 }
 # Azure AD
 provider "azuread" {
   version = ">=0.7.0"
   tenant_id       = "134888ca-e6f2-4fb6-9b49-3042d590ea87"
+  client_id = var.ARM_CLIENT_ID
+  client_secret = var.ARM_CLIENT_SECRET
 }
 
