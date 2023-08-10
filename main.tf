4d3dfdf2678dea86a72f7d1a18bd6fb950183350
unknown <bauerbrett1@gmail.com>
2023-08-09 20:51:12 -0400

    terraform test5

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "azurerm" {
   version = ">=2.0.0"
   features {}
   client_id       = "df100d44-e9e9-4296-bfc6-14e938aa47b7"
-  client_secret   = "lk68Q~w7C6uJOg4NiRLrUAFynHJW1NySQNcIoaNe"
+  client_secret   = "VX~8Q~T1lxb_sJyEVz~qVt7Po6p_rix3UQARIa61"
   subscription_id = "3b8667c6-8f75-42ea-b301-bf27c9db8674"
   tenant_id       = "134888ca-e6f2-4fb6-9b49-3042d590ea87"
 }
@@ -17,7 +17,7 @@ provider "azurerm" {
   alias = "timbdev"
   version = ">=2.0.0"
   client_id       = "df100d44-e9e9-4296-bfc6-14e938aa47b7"
-  client_secret   = "lk68Q~w7C6uJOg4NiRLrUAFynHJW1NySQNcIoaNe"
+  client_secret   = "VX~8Q~T1lxb_sJyEVz~qVt7Po6p_rix3UQARIa61"
   subscription_id = "c46a6ca5-2236-4b9d-aff2-e465190d0b3b"
   tenant_id       = "134888ca-e6f2-4fb6-9b49-3042d590ea87"
   skip_provider_registration = "true"
@@ -29,7 +29,7 @@ provider "azurerm" {
   version = ">=2.0.0"
   features {}
   client_id       = "df100d44-e9e9-4296-bfc6-14e938aa47b7"
-  client_secret   = "lk68Q~w7C6uJOg4NiRLrUAFynHJW1NySQNcIoaNe"
+  client_secret   = "VX~8Q~T1lxb_sJyEVz~qVt7Po6p_rix3UQARIa61"
   subscription_id = "50f1a1d8-806c-46ab-82f4-41fb54e730d8"
   tenant_id       = "134888ca-e6f2-4fb6-9b49-3042d590ea87"
   skip_provider_registration = "true"
@@ -38,7 +38,7 @@ provider "azurerm" {
 provider "azuread" {
   version = ">=0.7.0"
   client_id       = "df100d44-e9e9-4296-bfc6-14e938aa47b7"
-  client_secret   = "lk68Q~w7C6uJOg4NiRLrUAFynHJW1NySQNcIoaNe"
+  client_secret   = "VX~8Q~T1lxb_sJyEVz~qVt7Po6p_rix3UQARIa61"
   tenant_id       = "134888ca-e6f2-4fb6-9b49-3042d590ea87"
 }
 
